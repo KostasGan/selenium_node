903623de3998da29ad3ed6a1b932794bb1fff5af
KostasGan <kostasgan@e-food.gr>
2017-06-15 17:25:01 +0300

    finalize GetShopList testCase

--- test_cases/shoplist.js
+++ test_cases/shoplist.js
@@ -1,16 +1,26 @@
 let {By,until} = require('selenium-webdriver');
+let shops, count, shops_count;
 
-
-exports.GetShopList = (driver) =>{
-	driver.getCurrentUrl().then((current_url) => {
+exports.GetShopList = (driver) => { 
+	return driver.getCurrentUrl().then((current_url) => {
 		if((current_url.indexOf('/shops?user_address=') !== -1) || (current_url.indexOf('/shops?address=') !== -1)){
-			console.log('Contains');
+			shops = driver.findElement(By.className('shop-items'));
+			count = driver.findElement(By.css('span.number')).getText();
+			
+			let countToInt = Promise.resolve(count).then((count) => {return parseInt(count);});
+
+			if(countToInt !== 0){
+				shops.findElement(By.css('a[href*="/blue-shark"')).click();
+			}
 		}
 		else{
-		console.log('Doesn`t contains');
+			console.log('Wrong URL');
+			return false;
 		}
-
+	}).catch((e) => {
+		console.log(e);
+		return false;
 	});
-
-	
 };
+
+
