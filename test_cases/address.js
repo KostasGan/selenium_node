3c84112eb99e89e29483aa4419d789bc00b9b535
KostasGan <kostasgan@e-food.gr>
2017-06-30 17:41:42 +0300

    some styles

--- test_cases/address.js
+++ test_cases/address.js
@@ -1,32 +1,26 @@
 let {By,until} = require('selenium-webdriver');
-
+let address_field;
 
 exports.AddAddressAnonymous = (driver) => {
-	let address_field = driver.findElement(By.css('input.geosuggest__input'));
+	address_field = driver.findElement(By.css('div.geosuggest > div > input'));
 
-	return driver.wait(until.elementIsVisible(address_field), 5000)
-	.then(() => {
+	return driver.wait(until.elementIsVisible(address_field), 5000).then(() => {
 		address_field.sendKeys('Λεωφόρος ηρακλείου 409 Ηράκλειο');
-		return driver.wait(until.elementLocated(By.css('li.geosuggest-item.geosuggest-item')),5000)
-		.then(() => { 
-			let suggest = driver.findElement(By.css('li.geosuggest-item.geosuggest-item'));
-			suggest.click()
-			return driver.wait(until.elementLocated(By.css('form.address')),5000)
-			.then(() => {
+		return driver.wait(until.elementLocated(By.css('li.geosuggest-item.geosuggest-item')),5000).then(() => { 
+			driver.findElement(By.css('li.geosuggest-item.geosuggest-item')).click();
+
+			return driver.wait(until.elementLocated(By.css('form.address')),5000).then(() => {
 				let confirm_btn = driver.findElement(By.id('submit_btn'));
 				confirm_btn.click();
 				return true;
-			})
-			.catch((e) => {
+			}).catch((e) => {
 				console.log('No Map Confirmation Popup' + e);
 				return false;
 			});
-		})
-		.catch((e) => {
+		}).catch((e) => {
 			return false;
 		});
-	})
-	.catch((e) => {
+	}).catch((e) => {
 		console.log('No element visible' + e)
 		return false;
 	});
