b878030029b7d90a67b0a67360abc4a0b6093810
KostasGan <kostasgan@e-food.gr>
2017-07-31 13:48:31 +0300

    resolved conficts from merge

--- test_cases/address.js
+++ test_cases/address.js
@@ -2,18 +2,6 @@ let {By,until} = require('selenium-webdriver');
 let address_field;
 
 exports.AddAddressAnonymous = (driver) => {
-<<<<<<< HEAD
-	address_field = driver.findElement(By.css('div.geosuggest > div > input'));
-
-	return driver.wait(until.elementIsVisible(address_field), 5000).then(() => {
-		address_field.sendKeys('Λεωφόρος ηρακλείου 409 Ηράκλειο');
-		return driver.wait(until.elementLocated(By.css('li.geosuggest-item.geosuggest-item')),5000).then(() => { 
-			driver.findElement(By.css('li.geosuggest-item.geosuggest-item')).click();
-
-			return driver.wait(until.elementLocated(By.css('form.address')),5000).then(() => {
-				let confirm_btn = driver.findElement(By.id('submit_btn'));
-				confirm_btn.click();
-=======
 	return driver.wait(until.elementLocated(By.css('div.geosuggest > div > input')), 3000)
 	.then((address_field) => {
 		address_field.sendKeys('Αριστείδου 1 Μαρούσι');
@@ -23,7 +11,6 @@ exports.AddAddressAnonymous = (driver) => {
 			return driver.wait(until.elementLocated(By.css('form.address')),3000)
 			.then((formAddress) => {
 				formAddress.findElement(By.id('submit_btn')).click();
->>>>>>> master
 				return true;
 			}).catch((e) => {
 				console.log('No Map Confirmation Popup' + e);
