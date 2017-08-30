581940921cdf7b9a5722e4e68a85fdd2c0ac40c6
KostasGan <kostasgan@e-food.gr>
2017-08-30 15:45:19 +0300

    Add Offer functionality

--- test_cases/shop_profile.js
+++ test_cases/shop_profile.js
@@ -1,36 +1,36 @@
-let {By,until} = require('selenium-webdriver');
+let { By, until } = require('selenium-webdriver');
 const login = require('./login.js');
 const checkout = require('./checkout.js');
 const address = require('./address.js');
 
-function selectItemsOptions(driver){
+function selectItemsOptions(driver) {
 	return driver.wait(until.elementLocated(By.css('div#popup_menu_item'), 3000)).then((popup_menu) => {
 		driver.findElements(By.css('div.options.menu-item-options > div')).then((item_options) => {
-			if(item_options.length > 0) {
-				driver.findElements(By.css('div.option.menu-item-option.clearfix.required-option')).then((required)=>{
-					required.forEach((element) =>{
+			if (item_options.length > 0) {
+				driver.findElements(By.css('div.option.menu-item-option.clearfix.required-option')).then((required) => {
+					required.forEach((element) => {
 						input = driver.findElement(By.css('div.choice.menu-item-choice > input'));
 						driver.wait(until.elementIsSelected(input), 3000);
 					});
-				}).catch((e) => { console.log('Couldn`t find required options \n' + e ); });
+				}).catch((e) => { console.log('Couldn`t find required options \n' + e); });
 
-				driver.findElements(By.css('div.option.menu-item-option.clearfix:not(.required-option)')).then((NonRequired)=>{
+				driver.findElements(By.css('div.option.menu-item-option.clearfix:not(.required-option)')).then((NonRequired) => {
 					NonRequired.forEach((element1) => {
-						driver.wait(until.elementIsVisible(element1),3000).then((options) => {
+						driver.wait(until.elementIsVisible(element1), 3000).then((options) => {
 							options.findElement(By.css('div.ingredient-list > div:nth-child(2)')).then((option) => {
 								option.isEnabled().then((val) => {
-									if(val){
+									if (val) {
 										option.click();
 									}
 								});
 							});
 						});
 					});
-				}).catch((e) => { console.log('Couldn`t find non-required options \n' + e ); });
+				}).catch((e) => { console.log('Couldn`t find non-required options \n' + e); });
 			}
-			else{
+			else {
 				return false;
-			}	
+			}
 		}).catch((e) => { console.error('No Items Option Found \n' + e); });
 
 		driver.wait(until.elementLocated(By.css('span.button.button-primary')), 3000).then((button) => {
@@ -38,72 +38,83 @@ function selectItemsOptions(driver){
 				driver.findElement(By.css('div.spinner')).then((modal) => {
 					driver.wait(until.elementIsNotVisible(modal), 3000);
 				}).catch((e) => { console.log('Loader doesnt exist \n' + e); });
-			}).catch((e) => { console.log('Cannot find "Προσθηκη στο καλαθι" \n' + e); });		
+			}).catch((e) => { console.log('Cannot find "Προσθηκη στο καλαθι" \n' + e); });
 		});
-	}).catch((e) => { console.error('Item Modal hasn`t found \n' + e ); });
+	}).catch((e) => { console.error('Item Modal hasn`t found \n' + e); });
 }
 
 function AddItemToCart(driver) {
-	driver.findElement(By.id('IT_000000000416')).then((item) => {
+	driver.findElement(By.id('IT_000000000402')).then((item) => {
 		item.getAttribute('class').then((val) => {
-			if(val.indexOf('disabled') === -1){
+			if (val.indexOf('disabled') === -1) {
 				item.click();
 				driver.sleep(300);
 				selectItemsOptions(driver);
-
-				// driver.wait(until.elementLocated(By.id('addressbox')),1000).then(() => {
-				// 	address.AddAddressAnonymous(driver);
-				// 	driver.sleep(300);
-				// 	AddItemToCart(driver);
-				// }).catch((e) => { 
-				// 	console.log("Address exist. Flow Continue \n");
-				// 	driver.sleep(300);
-				// 	selectItemsOptions(driver);
-				// });
 			}
-			else{
+			else {
 				console.log('Item is Disable \n');
 				return;
 			}
 		});
-	}); 
-}
-
-function AddOffer(driver) {
-	driver.findElement(By.css('ul.offers-list > li')).then((offer) => {
-		offer.click();
-		driver.wait(until.elementLocated(By.id('popup_offer_wizard')), 1000).then((offer_modal) => {
-
-		});
-
 	});
 }
 
-exports.MakeOrderInShopProfile = (driver,creds) => {
-	return driver.getCurrentUrl().then((current_url) => {
-		if(current_url.indexOf('/delivery/neo-irakleio/blue-shark') !== -1 ){
+function AddOffer(driver) {
+	driver.sleep(250);
+	driver.wait(until.elementLocated(By.css('ul.offers-list > li:nth-child(3)')), 1000).then((offer) => {
+		offer.click();
+		driver.wait(until.elementLocated(By.id('popup_offer_wizard')), 1000).then((offer_modal) => {
+			driver.sleep(150);
+			driver.wait(until.elementsLocated(By.css('div.items > div')), 1000).then((items) => {
+				for (let i = 0; i < items.length; i++) {
+					driver.wait(until.elementLocated(By.css('div.items > div:nth-child(' + (i + 1) + ')')), 1000).then((item) => {
+						item.click();
+						item.findElement(By.css('option:nth-child(2)')).click().then(() => {
+							selectItemsOptions(driver);
+						}).catch((e) => { console.error('Kurwa 2 \n' + e); });
+					}).catch((e) => { console.error('Kurwa 1 \n' + e); });
+				}
+			}).catch((e) => { console.error('Kurwa \n' + e); });
+			driver.sleep(100);
+			driver.wait(until.elementLocated(By.css('div.submit.save > a.button')), 1000).then((button) => {
+				button.click().then(() => {
+					driver.findElement(By.css('div.spinner')).then((modal) => {
+						driver.wait(until.elementIsNotVisible(modal), 2000);
+					}).catch((e) => { console.log('Loader doesnt exist \n' + e); });
+				}).catch((e) => { console.log('Cannot find "Προσθηκη στο καλαθι" in Offer \n' + e); });
+			});
+		}).catch((e) => { console.log('Cannot find Offer Modal to continue \n' + e); });
+	}).catch((e) => { console.log('Cannot find Offers \n' + e); });
+}
 
-			driver.wait(until.elementLocated(By.css('div.order-steps div.hidden-xs button')),1000).then((button) => {
+exports.MakeOrderInShopProfile = (driver, creds) => {
+	return driver.getCurrentUrl().then((current_url) => {
+		if (current_url.indexOf('/delivery/neo-irakleio/blue-shark') !== -1) {
+
+			driver.wait(until.elementLocated(By.css('div.order-steps div.hidden-xs button')), 1000).then((button) => {
 				button.click();
 				address.AddAddressAnonymous(driver);
-			}).catch((e) => {});
+			}).catch((e) => { });
 
+			//AddItemToCart(driver);
 			AddItemToCart(driver);
-			AddItemToCart(driver);
+			AddOffer(driver);
+			//AddOffer(driver);
 
 			driver.findElements(By.css('div.cart-items > div')).then((cart_items) => {
-				if(cart_items.length > 0){
-					driver.findElement(By.id('continue-btn')).then((button)=>{
+				if (cart_items.length > 0) {
+					driver.findElement(By.id('continue-btn')).then((button) => {
 						button.isEnabled().then((value) => {
-							if(value){
+							if (value) {
+								driver.sleep(200);
 								button.click();
 								login.Login(driver, creds);
-								checkout.SubmitOrder(driver,creds.sms_pass);
+								checkout.SubmitOrder(driver, creds.sms_pass);
 							}
-							else{
-								driver.findElement(By.css('p.min-charge')).then((min_charge)=>{
+							else {
+								driver.findElement(By.css('p.min-charge')).then((min_charge) => {
 									min_charge.isDisplayed().then((val) => {
-										if(val){
+										if (val) {
 											AddItemToCart(driver);
 										}
 									});
