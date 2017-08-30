822195f0c07214f7bc11aaf54476f7c52533f9f1
KostasGan <kostasgan@e-food.gr>
2017-08-30 15:46:20 +0300

    code style and formation

--- test_cases/checkout.js
+++ test_cases/checkout.js
@@ -1,26 +1,26 @@
-let {By,until} = require('selenium-webdriver');
+let { By, until } = require('selenium-webdriver');
 const smsVerification = require('./smsVerification.js');
 let opt = null;
 
 function ValidateOrderInfo(driver) {
     return driver.findElement(By.css('div.order-content-wrapper')).then((OrderInfo) => {
-        driver.wait(until.elementLocated(By.id('customername')),1000).then((customername) => {
+        driver.wait(until.elementLocated(By.id('customername')), 1000).then((customername) => {
             customername.isDisplayed().then((val) => {
-                if(val){
+                if (val) {
                     customername.getAttribute('value').then((attr) => {
-                        if(attr === ''){
+                        if (attr === '') {
                             customername.sendKeys('Κώστας');
                         }
                     });
                 }
             });
-        }).catch((e) => { /*console.log('Can`t find customername input field \n' + e );*/});
+        }).catch((e) => { /*console.log('Can`t find customername input field \n' + e );*/ });
 
         driver.wait(until.elementLocated(By.css('input#customersurname')), 1000).then((customersurname) => {
             customersurname.isDisplayed().then((val) => {
-                if(val){
+                if (val) {
                     customersurname.getAttribute('value').then((attr) => {
-                        if(attr === ''){
+                        if (attr === '') {
                             customersurname.sendKeys('Γανώσης');
                         }
                     });
@@ -30,7 +30,7 @@ function ValidateOrderInfo(driver) {
 
         OrderInfo.findElement(By.css('input#doorbellname')).then((doorbell) => {
             doorbell.getAttribute('value').then((val) => {
-                if(val === ''){
+                if (val === '') {
                     doorbell.sendKeys('Δοκιμαστική111');
                 }
             });
@@ -38,51 +38,51 @@ function ValidateOrderInfo(driver) {
 
         OrderInfo.findElement(By.css('input#floor')).then((floor) => {
             floor.getAttribute('value').then((val) => {
-                if(val === ''){
+                if (val === '') {
                     floor.sendKeys('Δοκιμαστική');
                 }
             });
-        }).catch((e) => { console.log('Can`t find floor input field \n' + e ); });
+        }).catch((e) => { console.log('Can`t find floor input field \n' + e); });
 
         return OrderInfo.findElement(By.css('input#cellphone')).then((cellphone) => {
             return cellphone.isDisplayed('value').then((val) => {
-                if(val){
+                if (val) {
                     cellphone.getAttribute('value').then((attr) => {
-                        if(attr === ''){
+                        if (attr === '') {
                             cellphone.sendKeys('6981000000');
                         }
                     });
                     return 'need_validation';
                 }
             });
-        }).catch((e) => { console.log('Can`t find cellphone input field \n' + e ); });
-    }).catch((e) => { console.log('Can`t find Order Info form \n' + e ); });
+        }).catch((e) => { console.log('Can`t find cellphone input field \n' + e); });
+    }).catch((e) => { console.log('Can`t find Order Info form \n' + e); });
 }
 
 
-function SelectPaymentMethod(driver, option){
-    if(option === 'cash'){
+function SelectPaymentMethod(driver, option) {
+    if (option === 'cash') {
         driver.findElement(By.css('input#cash')).then((cash) => {
-            cash.isSelected().then((val)=>{
-                if(!val){
+            cash.isSelected().then((val) => {
+                if (!val) {
                     cash.click();
                 }
             });
         }).catch((e) => { console.log('Can`t find Cash Option \n' + e); });
     }
-    else if(option === 'credit'){
+    else if (option === 'credit') {
         driver.findElement(By.css('input#creditcard')).then((credit) => {
-            credit.isSelected().then((val)=>{
-                if(!val){
+            credit.isSelected().then((val) => {
+                if (!val) {
                     credit.click();
                 }
             });
         }).catch((e) => { console.log('Can`t find Credit Option \n' + e); });
     }
-    else if(option === 'paypal'){
+    else if (option === 'paypal') {
         driver.findElement(By.css('input#paypal')).then((paypal) => {
-            paypal.isSelected().then((val)=>{
-                if(!val){
+            paypal.isSelected().then((val) => {
+                if (!val) {
                     paypal.click();
                 }
             });
@@ -91,7 +91,7 @@ function SelectPaymentMethod(driver, option){
 }
 
 function AddCoupon(driver) {
-    driver.wait(until.elementLocated(By.css('div.order-content-wrapper')),300).then((info) => {
+    driver.wait(until.elementLocated(By.css('div.order-content-wrapper')), 300).then((info) => {
         info.findElement(By.id('coupon_code')).then((coupon) => {
             coupon.sendKeys('kgan');
 
@@ -99,11 +99,11 @@ function AddCoupon(driver) {
                 driver.wait(until.elementIsVisible(coupon_message), 3000).then((coupon_message) => {
                     coupon_message.getAttribute('innerHTML').then((msg) => {
                         console.log(msg);
-                        if(msg === 'Το κουπόνι είναι έγκυρο'){
+                        if (msg === 'Το κουπόνι είναι έγκυρο') {
                             console.log('Valid Coupon\n');
 
                             driver.findElement(By.id('coupon_discount')).isDisplayed().then((val) => {
-                                if(val)
+                                if (val)
                                     console.log('Right Message Appear in Cart \n');
                             }).catch((e) => { console.log('Coupon Message in Cart didnt appear \n' + e); });
                         }
@@ -116,22 +116,25 @@ function AddCoupon(driver) {
     });
 }
 
-exports.SubmitOrder = (driver,sms_pass) => {
+exports.SubmitOrder = (driver, sms_pass) => {
     driver.wait(until.urlContains('/orders/form?shop_id=968814'), 3000).then(() => {
         opt = ValidateOrderInfo(driver);
         //AddCoupon(driver);
-        SelectPaymentMethod(driver,'cash');
+        SelectPaymentMethod(driver, 'cash');
         driver.findElements(By.css('div.cart-items > div')).then((cart_items) => {
-            if(cart_items.length > 0){
+            if (cart_items.length > 0) {
                 driver.findElement(By.css('input#sendorder')).click().then(() => {
                     opt.then((opt) => {
-                        if(opt === 'need_validation'){
+                        if (opt === 'need_validation') {
                             smsVerification.phoneValidation(driver, sms_pass);
                         }
                     });
                 }).catch((e) => { console.log('Driver Cant Find SendOrder Button \n' + e); });
+
+                driver.wait(until.elementLocated(By.id('confirmationpopup')), 3000).then((confirm_popup) => {
+                    driver.wait(until.elementIsVisible(confirm_popup), 3000);
+                });
             }
         }).catch((e) => { console.log('Driver Cant find Cart Items \n' + e); });
-        
     }).catch((e) => { console.log('Current Page isnt Checkout. Something went wrong! \n' + e); });
 }
