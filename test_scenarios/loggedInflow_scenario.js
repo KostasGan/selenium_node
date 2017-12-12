a5ee01ff9363feb740e44594f517f600a706350f
KostasGan <kostasgan@e-food.gr>
2017-12-12 16:41:52 +0200

    styles and format

--- test_scenarios/loggedInflow_scenario.js
+++ test_scenarios/loggedInflow_scenario.js
@@ -8,10 +8,10 @@ const shopProfile = require('../test_cases/shop_profile');
 const checkout = require('../test_cases/checkout.js');
 
 exports.LoggedInFlow = (driver, chromeSettings, url, creds) => {
-	test.describe('LoggedIn Flow end-to-end Functionality Tests', function() {
+	test.describe('LoggedIn Flow end-to-end Functionality Tests', function () {
 		this.timeout(60000);
 
-		test.before(function() {
+		test.before(function () {
 			driver = new webdriver
 				.Builder()
 				.forBrowser('chrome')
@@ -20,38 +20,38 @@ exports.LoggedInFlow = (driver, chromeSettings, url, creds) => {
 
 			driver.manage().window().maximize();
 		});
-		test.beforeEach(function(){
+		test.beforeEach(function () {
 			driver.getWindowHandle();
 		})
-		test.after(function() {
+		test.after(function () {
 			driver.quit();
 		});
-		test.it('Successfully Login', function() {
+		test.it('Successfully Login', function () {
 			driver.get(url);
 
 			login.Login(driver, creds).then((val) => {
 				assert.ok(val);
-            });
+			});
 		});
-		test.it('Select Address', function() {
-            address.SelectAddress(driver).then((val) => {
+		test.it('Select Address', function () {
+			address.SelectAddress(driver).then((val) => {
 				assert.equal(val, 'Completed');
-            });
+			});
 		});
-		test.it('Select Blue Shark For Shop Listing', function() {
-            shoplist.GetShopList(driver).then((val) => {
+		test.it('Select Blue Shark For Shop Listing', function () {
+			shoplist.GetShopList(driver).then((val) => {
 				assert.equal(val, 'Completed');
-            });
+			});
 		});
-		test.it('Shop Profile - Add Items/Offer To Cart', function() {
-            shopProfile.MakeOrderInShopProfile(driver,creds).then((val) => {
+		test.it('Shop Profile - Add Items/Offer To Cart', function () {
+			shopProfile.MakeOrderInShopProfile(driver, creds).then((val) => {
 				assert.equal(val, 'Completed');
-            });
+			});
 		});
-		test.it('Checkout and GO', function() {
-            checkout.SubmitOrder(driver,creds).then((val) => {
+		test.it('Checkout and GO', function () {
+			checkout.SubmitOrder(driver, creds).then((val) => {
 				assert.equal(val, 'Completed');
-            });
+			});
 		});
 	});
 };
