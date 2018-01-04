418cd6e218f08c14dc11db150c3612fea8b1367b
KostasGan <kostasgan@e-food.gr>
2018-01-04 16:53:01 +0200

    refactor Register Functionality

--- test_cases/register.js
+++ test_cases/register.js
@@ -1,38 +1,52 @@
 let {By,until} = require('selenium-webdriver');
 
 function RegisterModalFunc(driver,creds) {
-	driver.wait(until.elementLocated(By.id('register-form')), 1000).then((form) => {
-		form.findElement(By.id('register_email')).sendKeys('ad' + creds.email); 
-		form.findElement(By.id('register_pass')).sendKeys(creds.pass); 
-		form.findElement(By.id('register_pass_repeat')).sendKeys(creds.pass);
-		return driver.findElement(By.css('button.button.button-primary.register-form-submit')).click().then(() => {	
+	return driver.wait(until.elementLocated(By.id('register-form')), 1000).then((form) => {
+		driver.wait(until.elementIsVisible(form), 1500);
+		
+		driver.wait(until.elementIsVisible(form.findElement(By.id('register_email'))), 2000).then((emailField) => {
+			emailField.sendKeys(creds.email);
+		});
+		driver.wait(until.elementIsVisible(form.findElement(By.id('register_pass'))), 2000).then((emailField) => {
+			emailField.sendKeys(creds.pass);
+		});
+		driver.wait(until.elementIsVisible(form.findElement(By.id('register_pass_repeat'))), 2000).then((emailField) => {
+			emailField.sendKeys(creds.pass);
+		});
+
+		return driver.findElement(By.css('button.register-form-submit')).then((button) => {
+			button.click();
+			
 			return driver.wait(until.elementLocated(By.id('userlink')), 4000).then((logged_name) => {
 				driver.wait(until.elementIsVisible(logged_name), 2000);
-				return 'true';
+				return 'Completed';
 			}).catch((e)=>{
 				console.log('Something bad happen \n' + e);
-				return 'false';
+				return 'Failed';
 			});
 		});
+	}).catch((e) => {
+		console.log('Cannot Find the registration-form \n' + e);
+		return 'Failed';
 	});
 }
 
 exports.Register = (driver,creds) => {
-	driver.wait(until.elementLocated(By.css('div.modal-content')), 1000).then((modal) => {
-		driver.wait(until.elementIsVisible(modal),2000).then(() => {
-			driver.sleep(350);
-			driver.findElement(By.css('a[href*="#register-form')).click().then(() => {
-				RegisterModalFunc(driver,creds);
+	return driver.wait(until.elementLocated(By.css('div.modal-content')), 1000).then((modal) => {
+		return driver.wait(until.elementIsVisible(modal),2000).then(() => {
+			driver.wait(until.elementIsNotVisible(driver.findElement(By.css('div.is-loading'))), 1500);
+			return driver.findElement(By.css('a[href*="#register-form')).click().then(() => {
+				return RegisterModalFunc(driver,creds);
 			});
 		});
 	}).catch(() => {
 		console.log('Login/Register Modal didn`t open. Continue by pressing Register Button. \n');
-		driver.findElement(By.css('div.col-md-3.col-sm-4 > a')).click();
-		driver.wait(until.elementLocated(By.css('div.modal-content')), 1000).then((modal) => {
-			driver.wait(until.elementIsVisible(modal),2000).then(() => {
-				driver.sleep(350);
-				driver.findElement(By.css('a[href*="#register-form')).click().then(() => {
-					RegisterModalFunc(driver,creds);
+		driver.findElement(By.css('a.user-login')).click();
+		return driver.wait(until.elementLocated(By.css('div.modal-content')), 1000).then((modal) => {
+			return driver.wait(until.elementIsVisible(modal),2000).then(() => {
+				driver.wait(until.elementIsNotVisible(driver.findElement(By.css('div.is-loading'))), 1500);
+				return driver.findElement(By.css('a[href*="#register-form')).click().then(() => {
+					return RegisterModalFunc(driver,creds);
 				});
 			});
 		});
