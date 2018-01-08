cc940912a679eabac3921fb950d543dbc9c7f398
KostasGan <kostasgan@e-food.gr>
2018-01-08 15:36:07 +0200

    created 2 scenarios for anonymous

--- test_scenarios/anonymousflow_scenario.js
+++ test_scenarios/anonymousflow_scenario.js
@@ -7,10 +7,63 @@ const shoplist = require('../test_cases/shoplist');
 const shopProfile = require('../test_cases/shop_profile');
 const checkout = require('../test_cases/checkout.js');
 
-exports.AnonymousFlow = (driver, chromeSettings, url, creds) => {
-    test.describe('Anonymous Flow end-to-end Functionality Tests', function () {
-        this.timeout(60000);
 
+exports.AnonymousFlow = (driver, chromeSettings, url, creds) => {
+    // test.describe('Anonymous Flows', function () {
+    //     this.timeout(35000);
+    //     this.slow(10000);
+
+    //     test.before(function () {
+    //         driver = new webdriver
+    //             .Builder()
+    //             .forBrowser('chrome')
+    //             .setChromeOptions(chromeSettings)
+    //             .build();
+    
+    //         driver.get(url);
+    //     });
+    //     test.after(function () {
+    //         driver.quit();
+    //     });
+
+    //     test.describe("Homepage to Checkout(Login)", function () {
+
+    //         test.it('Add Address', function () {
+                
+    //             address.AddAddressAnonymous(driver).then((val) => {
+    //                 assert.ok(val);
+    //             });
+    //         });
+    //         test.it('Select Blue Shark For Shop Listing', function () {
+    //             shoplist.GetShopList(driver).then((val) => {
+    //                 assert.equal(val, 'Completed');
+    //             });
+    //         });
+           
+    //         test.it('Shop Profile - Add Items/Offer To Cart', function () {
+    //             shopProfile.MakeOrderInShopProfile(driver, creds).then((val) => {
+    //                 assert.equal(val, 'Completed');
+    //             });
+    //         });
+            
+            
+    //         test.it('Successfully Login', function () {
+    //             login.Login(driver, creds).then((val) => {
+    //                 assert.ok(val);
+    //             });
+    //         });
+    //         test.it('Checkout and GO', function () {
+    //             checkout.SubmitOrder(driver, creds).then((val) => {
+    //                 assert.equal(val, 'Completed');
+    //             });
+    //         });
+    //     });
+    // });
+    
+    test.describe('Anonymous Flows 3', function () {
+        this.timeout(35000);
+        this.slow(10000);
+        
         test.before(function () {
             driver = new webdriver
                 .Builder()
@@ -18,41 +71,41 @@ exports.AnonymousFlow = (driver, chromeSettings, url, creds) => {
                 .setChromeOptions(chromeSettings)
                 .build();
 
-            driver.manage().window().maximize();
+            driver.get(url + "/delivery/neo-irakleio/blue-shark");
         });
-        test.beforeEach(function () {
-            driver.getWindowHandle();
-        })
         test.after(function () {
             driver.quit();
         });
 
-        test.it('Add Address', function () {
-            driver.get(url);
+        test.describe("Shop Profile to Checkout(Login)", function () {
+            test.it('Check for no Address Popup', function () {
+                shopProfile.CheckForNoAddress(driver).then((val) => {
+                    assert.ok(val);
+                });
+            });
 
-            address.AddAddressAnonymous(driver).then((val) => {
-                assert.ok(val);
+            test.it('Add Address', function () {
+                address.AddAddressAnonymous(driver).then((val) => {
+                    assert.ok(val);
+                });
+            });
+           
+            test.it('Shop Profile - Add Items/Offer To Cart', function () {
+                shopProfile.MakeOrderInShopProfile(driver, creds).then((val) => {
+                    assert.equal(val, 'Completed');
+                });
+            });
+
+            test.it('Successfully Login', function () {
+                login.Login(driver, creds).then((val) => {
+                    assert.ok(val);
+                });
+            });
+            test.it('Checkout and GO', function () {
+                checkout.SubmitOrder(driver, creds).then((val) => {
+                    assert.equal(val, 'Completed');
+                });
             });
         });
-        // test.it('Select Blue Shark For Shop Listing', function () {
-        //     shoplist.GetShopList(driver).then((val) => {
-        //         assert.equal(val, 'Completed');
-        //     });
-        // });
-        // test.it('Shop Profile - Add Items/Offer To Cart', function () {
-        //     shopProfile.MakeOrderInShopProfile(driver, creds).then((val) => {
-        //         assert.equal(val, 'Completed');
-        //     });
-        // });
-        // test.it('Successfully Login', function () {
-        //     login.Login(driver, creds).then((val) => {
-        //         assert.ok(val);
-        //     });
-        // });
-        // test.it('Checkout and GO', function () {
-        //     checkout.SubmitOrder(driver, creds).then((val) => {
-        //         assert.equal(val, 'Completed');
-        //     });
-        // });
     });
 };
