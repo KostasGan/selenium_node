3ddcd243bfeb057e718b9e80b3b3d1decd8657ae
KostasGan <kostasgan@e-food.gr>
2018-01-08 15:42:09 +0200

    remove comments

--- test_scenarios/anonymousflow_scenario.js
+++ test_scenarios/anonymousflow_scenario.js
@@ -9,56 +9,56 @@ const checkout = require('../test_cases/checkout.js');
 
 
 exports.AnonymousFlow = (driver, chromeSettings, url, creds) => {
-    // test.describe('Anonymous Flows', function () {
-    //     this.timeout(35000);
-    //     this.slow(10000);
+    test.describe('Anonymous Flows', function () {
+        this.timeout(35000);
+        this.slow(10000);
 
-    //     test.before(function () {
-    //         driver = new webdriver
-    //             .Builder()
-    //             .forBrowser('chrome')
-    //             .setChromeOptions(chromeSettings)
-    //             .build();
+        test.before(function () {
+            driver = new webdriver
+                .Builder()
+                .forBrowser('chrome')
+                .setChromeOptions(chromeSettings)
+                .build();
     
-    //         driver.get(url);
-    //     });
-    //     test.after(function () {
-    //         driver.quit();
-    //     });
+            driver.get(url);
+        });
+        test.after(function () {
+            driver.quit();
+        });
 
-    //     test.describe("Homepage to Checkout(Login)", function () {
+        test.describe("Homepage to Checkout(Login)", function () {
 
-    //         test.it('Add Address', function () {
+            test.it('Add Address', function () {
                 
-    //             address.AddAddressAnonymous(driver).then((val) => {
-    //                 assert.ok(val);
-    //             });
-    //         });
-    //         test.it('Select Blue Shark For Shop Listing', function () {
-    //             shoplist.GetShopList(driver).then((val) => {
-    //                 assert.equal(val, 'Completed');
-    //             });
-    //         });
+                address.AddAddressAnonymous(driver).then((val) => {
+                    assert.ok(val);
+                });
+            });
+            test.it('Select Blue Shark For Shop Listing', function () {
+                shoplist.GetShopList(driver).then((val) => {
+                    assert.equal(val, 'Completed');
+                });
+            });
            
-    //         test.it('Shop Profile - Add Items/Offer To Cart', function () {
-    //             shopProfile.MakeOrderInShopProfile(driver, creds).then((val) => {
-    //                 assert.equal(val, 'Completed');
-    //             });
-    //         });
+            test.it('Shop Profile - Add Items/Offer To Cart', function () {
+                shopProfile.MakeOrderInShopProfile(driver, creds).then((val) => {
+                    assert.equal(val, 'Completed');
+                });
+            });
             
             
-    //         test.it('Successfully Login', function () {
-    //             login.Login(driver, creds).then((val) => {
-    //                 assert.ok(val);
-    //             });
-    //         });
-    //         test.it('Checkout and GO', function () {
-    //             checkout.SubmitOrder(driver, creds).then((val) => {
-    //                 assert.equal(val, 'Completed');
-    //             });
-    //         });
-    //     });
-    // });
+            test.it('Successfully Login', function () {
+                login.Login(driver, creds).then((val) => {
+                    assert.ok(val);
+                });
+            });
+            test.it('Checkout and GO', function () {
+                checkout.SubmitOrder(driver, creds).then((val) => {
+                    assert.equal(val, 'Completed');
+                });
+            });
+        });
+    });
     
     test.describe('Anonymous Flows 3', function () {
         this.timeout(35000);
