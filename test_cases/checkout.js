1218bd2e628484e3873beb4bedd35e8732864dbc
KostasGan <kostasgan@e-food.gr>
2018-02-07 16:32:05 +0200

    some refacts

--- test_cases/checkout.js
+++ test_cases/checkout.js
@@ -116,30 +116,33 @@ exports.AddCoupon = function(driver) {
     });
 }
 
+
+exports.WaitForOrderConfirmation = (driver) => {
+    return driver.wait(until.elementLocated(By.id('confirmationpopup')), 3000).then((confirm_popup) => {
+        driver.wait(until.elementIsVisible(confirm_popup), 3000);
+        driver.wait(until.elementIsVisible(confirm_popup.findElement(By.css('section.modal-text'))), 15000);
+        return 'Completed';
+    });
+}
+
 exports.SubmitOrder = (driver, sms_pass) => {
     return driver.wait(until.urlContains('/orders/form?shop_id=968814'), 5000).then(() => {
         opt = ValidateOrderInfo(driver);
         SelectPaymentMethod(driver, 'cash');
         return driver.findElements(By.css('div.cart-items')).then((cart_items) => {
             if (cart_items.length > 0) {
-                driver.wait(until.elementLocated(By.css('fieldset.submit-set')), 1500).then((submit) => {
-                    driver.executeAsyncScript("var callback = arguments[arguments.length - 1]; arguments[0].scrollIntoView(top); callback();", submit).then(() => {
+                return driver.wait(until.elementLocated(By.css('fieldset.submit-set')), 1500).then((submit) => {
+                    return driver.executeAsyncScript("var callback = arguments[arguments.length - 1]; arguments[0].scrollIntoView(top); callback();", submit).then(() => {
                         submit.findElement(By.id('sendorder')).click();
-                        
-                        opt.then((opt) => {
-                            if (opt === 'need_validation') {
-                                smsVerification.phoneValidation(driver, sms_pass);
-                            }
-                        });
+                        return 'Completed';
+                        // opt.then((opt) => {
+                        //     if (opt === 'need_validation') {
+                        //         smsVerification.phoneValidation(driver, sms_pass);
+                        //     }
+                        // });
                     });
                     
                 }).catch((e) => { console.log('Driver Cant Find SendOrder Button \n' + e); });
-
-                return driver.wait(until.elementLocated(By.id('confirmationpopup')), 3000).then((confirm_popup) => {
-                    driver.wait(until.elementIsVisible(confirm_popup), 3000);
-                    driver.wait(until.elementIsVisible(confirm_popup.findElement(By.css('section.modal-text'))), 15000);
-                    return 'Completed';
-                });
             }
         }).catch((e) => { console.log('Driver Cant find Cart Items \n' + e); });
     }).catch((e) => { console.log('Current Page isnt Checkout. Something went wrong! \n' + e); });
