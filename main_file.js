1a11e7e4bc571d0097f2e1ddc80f2810bf75a3b1
KostasGan <kostasgan@e-food.gr>
2018-03-02 18:11:11 +0200

    support notification scenario

--- main_file.js
+++ main_file.js
@@ -7,13 +7,13 @@ const logout_scenarios = require('./test_scenarios/logout_scenario').Logout;
 const loggedInFlow_scenarios = require('./test_scenarios/loggedInflow_scenario').LoggedInFlow;
 const register_scenarios = require('./test_scenarios/register_scenario').Register;
 const anonymousflow_scenarios = require('./test_scenarios/anonymousflow_scenario').AnonymousFlow;
-
+const notification_scenarios = require('./test_scenarios/notification_scenario').NotificationCheck;
 
 //configs 
 let ngcreds = config.get('Env.ngusrnm') + ':' + config.get('Env.ngpass') + '@';
 let main_url = config.get('Env.url');
 let url = 'https://' + ngcreds + main_url;
-let driver, chromeSettings = new chrome.Options().addArguments(config.get('ChromeSettings'));
+let driver, chromeSettings = new chrome.Options().addArguments();
 let creds = {
 	'email': config.get('User.email'),
 	'pass': config.get('User.pass'),
@@ -25,3 +25,4 @@ login_scenarios(driver, chromeSettings, url, creds);
 logout_scenarios(driver, chromeSettings, url, creds);
 loggedInFlow_scenarios(driver, chromeSettings, url, creds);
 anonymousflow_scenarios(driver, chromeSettings, url, creds);
+notification_scenarios(driver,chromeSettings, url, creds);
